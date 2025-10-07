d44d8069a8cb6e373e8e166685f14af745243105
Rodrigo Llull Torres <rllullt@gmail.com>
2025-10-06 23:36:22 -0300

    Insert Person function

--- src/CRUD/Query.hs
+++ src/CRUD/Query.hs
@@ -4,14 +4,14 @@
 
 module CRUD.Query where
 
-import Database.SQLite.Simple (Connection, open, FromRow, query_)
+import Database.SQLite.Simple (Connection, FromRow, ToRow, open, close, execute, query_)
 import GHC.Generics
 
 data Person = Person {
     personId   :: Int,
     personName :: String,
     age        :: Int
-} deriving (Eq, Show, FromRow, Generic)
+} deriving (Eq, Show, FromRow, ToRow, Generic)
 
 getConn :: IO Connection
 getConn = open "./haskell-servantDB"
@@ -20,7 +20,24 @@ fetchPersonsQ :: IO [Person]
 fetchPersonsQ = do
     conn <- getConn
     personList <- query_ conn "SELECT * FROM persons;"
+    close conn
     pure personList
+
+insertPersonQ :: Person -> IO ()
+insertPersonQ person = do
+    conn <- getConn
+    execute conn "INSERT INTO persons (person_id, person_name, age) VALUES (?, ?, ?)" person --(personName person, age person)
+    close conn
+
+-- updatePersonQ :: Person -> IO ()
+-- updatePersonQ person = do
+--     conn <- getConn
+--     execute conn "UPDATE persons SET person_name = ?, age = ? WHERE person_id = ?" (personName, age, personId)
+--     close conn
+--     where
+--         personId = personId person
+--         personName = personName person
+--         age = age person
 
 test :: IO ()
 test = putStrLn "Hello from Query"
